e36472a053026eef26b6e2a8a8e07fead80fd815
mason <1606227397@qq.com>
2024-05-11 10:13:32 +0800

    更新源

--- main.py
+++ main.py
@@ -98,23 +98,62 @@ def getQA_YouXi369_49969(soup, month, day):
     return message_all
 
 
+def getQA_ALi213_371835(soup, month, day):
+    message_all = []
+
+    count = 0
+
+    todayNo1 = '【' + month + '.' + day + '】'
+
+    ps = soup.find_all('p')
+    for p_ele in ps:
+        if count >= 20:
+            break
+
+        if todayNo1 in p_ele.text:
+            message_all.append(todayNo1 + '\n')
+            print(todayNo1)
+            answer1 = p_ele.find_next_sibling('p')
+            if answer1:
+                message_all.append(answer1.text + '\n')
+                print(answer1.text)
+                answer2 = answer1.find_next_sibling('p')
+                if answer2:
+                    message_all.append(answer2.text + '\n')
+                    print(answer2.text)
+
+        count += 1
+    return message_all
+
+
 def getSoup(url):
-    # get请求获取内容
-    response = requests.get(url)
+    # 设置超时时间为10秒
+    timeout = 10
 
-    # 编码
-    response.encoding = response.apparent_encoding
+    try:
+        # get请求获取内容
+        response = requests.get(url, timeout=timeout)
 
-    # 取text属性
-    html_content = response.text
-    print(url)
-    print(html_content)
+        # 编码
+        response.encoding = response.apparent_encoding
 
-    # 解析HTML
-    soup = BeautifulSoup(html_content, 'html.parser')
+        # 取text属性
+        html_content = response.text
+        print(url)
+        print(html_content)
 
-    return soup
+        # 解析HTML
+        soup = BeautifulSoup(html_content, 'html.parser')
 
+        return soup
+    except requests.exceptions.Timeout:
+        print('请求超时')
+        soup = BeautifulSoup('', 'html.parser')
+        return soup
+    except requests.exceptions.RequestException as e:
+        print(e)
+        soup = BeautifulSoup('', 'html.parser')
+        return soup
 def main():
     now = datetime.now()
 
@@ -128,12 +167,16 @@ def main():
 
     url1 = 'https://m.duotegame.com/mgl/35822.html'
     url2 = 'https://www.youxi369.com/gonglue/49969.html'
+    url3 = 'https://app.ali213.net/mip/gl/371835.html'
 
     soup = getSoup(url1)
     message_all = getQA_DuoTe_35822(soup, month, day)
     if not message_all:
         soup = getSoup(url2)
         message_all = getQA_YouXi369_49969(soup, month, day)
+    if not message_all:
+        soup = getSoup(url3)
+        message_all = getQA_ALi213_371835(soup, month, day)
 
     bark_deviceKey = os.environ.get('BARK_DEVICEKEY')
     bark_icon = os.environ.get('BARK_ICON')
